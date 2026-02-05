3e59a783a4688240db4c7fe0a995f71b3480b30e
NorNoteBK <156896017+NorNoteBK@users.noreply.github.com>
2026-02-05 15:04:30 +0700

    Fix formatting in database settings

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'USER': 'note',
         'PASSWORD': '5^gnukytetoN',
         'HOST': 'noppasin-sql.database.windows.net',
-        'PORT': '1433',
+        'PORT': '1433', 
         'OPTIONS': {
             'driver': 'ODBC Driver 18 for SQL Server',
         },
